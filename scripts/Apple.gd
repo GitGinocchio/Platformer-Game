d6f4b408ba024d26266027cc3f53a231a67df194
GitGinocchio <giuliotognetto2@gmail.com>
2024-08-09 13:49:45 +0200

    random collectables

--- scripts/Apple.gd
+++ scripts/Apple.gd
@@ -3,6 +3,32 @@ extends Area2D
 
 @onready var sprite = $AnimatedSprite2D
 
+@export var Sprite_Frames : SpriteFrames = null
+@export var Randomize : bool = false
+
+const Collectables_Path = "res://resources/collectables/"
+
+@onready var collectables = [
+	preload('res://resources/collectables/apple.tres'),
+	preload('res://resources/collectables/banana.tres'),
+	preload('res://resources/collectables/cherrie.tres'),
+	preload('res://resources/collectables/melon.tres'),
+	preload('res://resources/collectables/orange.tres'),
+	preload('res://resources/collectables/pineapple.tres'),
+	preload('res://resources/collectables/strawberrie.tres'),
+	preload('res://resources/collectables/wiki.tres')
+]
+
+func _ready() -> void:
+	if Sprite_Frames and not Randomize:
+		sprite.sprite_frames = Sprite_Frames
+		sprite.play('idle')
+	elif Randomize:
+		sprite.sprite_frames = collectables.pick_random()
+		sprite.play('idle')
+	else:
+		queue_free()
+
 func _on_body_entered(body):
 	if body.is_in_group("Player"): sprite.play("collected")
 
