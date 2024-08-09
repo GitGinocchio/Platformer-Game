067c2f0ad633d3a48a6631201e301a9aba16590f
GitGinocchio <giuliotognetto2@gmail.com>
2024-08-09 16:27:58 +0200

    recreated spikes, created saw

--- scripts/Saw.gd
+++ scripts/Saw.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,6 +11,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+
 func _on_trigger_area_body_entered(body: Node2D) -> void:
-	if body.is_in_group("Player"):
+	if body.is_in_group("Player") and sprite.animation == 'on':
 		body.call_deferred("die")
