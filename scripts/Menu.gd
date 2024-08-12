cb27a6b369cf4078428a7fd04d52f491c0204422
Deniel <deshbertolesh@gmail.com>
2024-08-12 02:55:37 +0200

    update menu
    reminder: had to finish the options menu

--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -11,13 +11,12 @@ extends Control
 	preload("res://assets/Background/Yellow.png")
 ]
 
-
 func _ready() -> void:
 	background.texture = backgrounds.pick_random()
 
-func _on_play_pressed():
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
+func _on_new_game_pressed():
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 
 func _on_options_pressed():
